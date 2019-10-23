5d75bd17dc8d7dd67f4b4197fd3f08468340b6d6
Luxri <lucas.nasstrom@elev.ga.ntig.se>
2019-10-23 11:00:10 +0200

    Background, logo

--- php/views/index_layout.php
+++ php/views/index_layout.php
@@ -6,8 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= $pageTitle; ?>Hans och Greta</title>
     <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Oswald&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
-<body>
+<body background="css/images/dark woods.jpg">
+   <header>
+        <img src="css/images/Hans och Greta 2.svg" alt="H&G Logo" width="300px" height="200px">
+   </header>
     <main>
         <p>
             <?= $story['body'] ?>
@@ -15,11 +20,16 @@
 
         <ul>
             <?php foreach ($links as $link): ?>
+            <button type="button" class="btn btn-outline-danger">
                 <a href="index.php?id=<?= $link['target_id'] ?>">
-                <li><?= $link['description'] ?></li>
+                    <li><?= $link['description'] ?></li>
                 </a>
+            </button>
             <?php endforeach ?>
         </ul>
     </main>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
